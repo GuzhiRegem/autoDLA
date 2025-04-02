08ee201160823be639bb29f67cf1a7f2ee1e4744
GuzhiRegem <sebastian.moreiradl@gmail.com>
2025-04-02 14:26:45 -0300

    get_json_schema list flag

--- autodla/engine/db.py
+++ autodla/engine/db.py
@@ -54,6 +54,8 @@ class DB_Connection:
                     class_out[k]["primary_key"] = True
                 if "depends" in f:
                     class_out[k]["depends"] = f'$ref:{f["depends"].__name__}'
+                if "is_list" in f:
+                    class_out[k]["is_list"] = f["is_list"]
                 type_st = f["type"].__name__
                 if get_origin(f['type']) == list:
                     arg = get_args(f["type"])
