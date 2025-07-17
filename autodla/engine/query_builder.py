9df22fc7e2b5311e09652bf88d9700c85af367fb
GuzhiRegem <sebastian.moreiradl@gmail.com>
2025-07-17 10:16:56 -0300

    typing progress

--- autodla/engine/query_builder.py
+++ autodla/engine/query_builder.py
@@ -2,24 +2,56 @@ import polars as pl
 from typing import Callable, List, Optional
 from autodla.engine.data_conversion import DataTransformer
 
+
 class QueryBuilder:
-    def __init__(self, data_transformer = DataTransformer):
+    def __init__(self, data_transformer: DataTransformer):
         self._data_transformer = data_transformer
 
-    def select(self, from_table: str, columns: List[str], where: str = None, limit: int = 10, order_by: str = None, group_by: list[str] = None, offset: int = None) -> pl.DataFrame:
+    def select(
+            self,
+            from_table: str,
+            columns: List[str],
+            where: Optional[str] = None,
+            limit: Optional[int] = 10,
+            order_by: Optional[str] = None,
+            group_by: Optional[list[str]] = None,
+            offset: Optional[int] = None
+    ) -> pl.DataFrame:
+        return pl.DataFrame()
+
+    def insert(
+            self,
+            into_table: str,
+            values: List[dict]
+    ) -> None:
         pass
 
-    def insert(self, into_table: str, values: List[dict]) -> None:
+    def update(
+            self,
+            table: str,
+            values: dict,
+            where: str
+    ) -> None:
         pass
 
-    def update(self, table: str, values: dict, where: str) -> None:
+    def delete(
+            self,
+            table: str,
+            where: str
+    ) -> None:
         pass
 
-    def delete(self, table: str, where: str) -> None:
+    def create_table(
+            self,
+            table_name: str,
+            schema: dict,
+            if_exists: bool = False
+    ) -> None:
         pass
 
-    def create_table(self, table_name: str, schema: dict, if_exists = False) -> None:
-        pass
-
-    def drop_table(self, table_name: str, if_exists = False) -> None:
+    def drop_table(
+            self,
+            table_name: str,
+            if_exists: bool = False
+    ) -> None:
         pass
